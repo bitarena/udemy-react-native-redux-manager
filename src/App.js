cd6492606503a979e3703d58b969e967314701c7
David Muro <david.muro9@gmail.com>
2018-01-09 22:58:52 +0100

    added boiler plate code

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import config from './authConfig';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/authConfig.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyCpwX1O8Qm0q_HoVSYu7-i76ogS98YUmKQ',
+  authDomain: 'udemy-manager-e6520.firebaseapp.com',
+  databaseURL: 'https://udemy-manager-e6520.firebaseio.com',
+  projectId: 'udemy-manager-e6520',
+  storageBucket: 'udemy-manager-e6520.appspot.com',
+  messagingSenderId: '195861622856'
+};
+
+export default config;
